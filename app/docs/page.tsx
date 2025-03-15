b2beb7423cc5fd4602cb94867543a1ff5fe38356
Arash <arashn2y@gmail.com>
2025-03-15 11:21:03 +0100

    fix docs page

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -6,15 +6,13 @@ const RedocStandalone = dynamic(() => import("redoc").then(mod => mod.RedocStand
 
 const RedocPage = () => {
   return (
-    <main className="flex flex-col items-center justify-center">
-      <RedocStandalone
-        specUrl="/api/docs"
-        options={{
-          theme: { colors: { primary: { main: "#6EC5AB" } } },
-          expandResponses: "200"
-        }}
-      />
-    </main>
+    <RedocStandalone
+      specUrl="/api/docs"
+      options={{
+        theme: { colors: { primary: { main: "#6EC5AB" } } },
+        expandResponses: "200"
+      }}
+    />
   );
 };
 
