f4d5e261ed33b7df05cca0e850906aa9c68c3d7c
Arash <arashn2y@gmail.com>
2025-03-15 11:18:39 +0100

    fix

--- env/schema.ts
+++ env/schema.ts
@@ -1,11 +1,9 @@
 import { z } from "zod";
 
 export const serverEnvSchema = z.object({
-	DATABASE_URL: z.string().url(),
-	JWT_SECRET: z.string(),
-	ALLOWED_DOMAIN: z.string(),
-	ARCJET_ENV: z.string(),
-	ARCJET_KEY: z.string(),
-	PORT: z.string(),
-	PEPPER: z.string(),
+  DATABASE_URL: z.string().url(),
+  JWT_SECRET: z.string(),
+  ALLOWED_DOMAIN: z.string(),
+  ARCJET_KEY: z.string(),
+  PEPPER: z.string()
 });
